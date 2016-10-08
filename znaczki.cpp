cc9a5ea4186270706fa9c45c79364ec33d401ee0
mizworski <myxxeh@gmail.com>
2016-10-08 22:24:23 +0200

    Refactoring.

--- znaczki.cpp
+++ znaczki.cpp
@@ -3,63 +3,49 @@
 #include <set>
 #include <algorithm>
 
-#define stamp_key std::tuple<int, std::string, std::pair <std::string, double>, std::string>
-
-int count = 0;
+const int year = 0;
+const int post_office_name = 1;
+const int value = 2;
+const int stamp_name = 3;
 
 struct compare {
-    bool operator()(const stamp_key lhs,
-                    const stamp_key rhs) const {
+    bool operator()(const std::tuple<int, std::string, std::pair<std::string, double>, std::string> lhs,
+                    const std::tuple<int, std::string, std::pair<std::string, double>, std::string> rhs) const {
         bool lesser = true;
 
-        lesser = std::get<3>(lhs) < std::get<3>(rhs) ? true :
-                 std::get<3>(lhs) > std::get<3>(rhs) ? false : lesser;
-        lesser = std::get<2>(lhs).second < std::get<2>(rhs).second ? true :
-                 std::get<2>(lhs).second > std::get<2>(rhs).second ? false : lesser;
-        lesser = std::get<1>(lhs) < std::get<1>(rhs) ? true :
-                 std::get<1>(lhs) > std::get<1>(rhs) ? false : lesser;
-        lesser = std::get<0>(lhs) < std::get<0>(rhs) ? true :
-                 std::get<0>(lhs) > std::get<0>(rhs) ? false : lesser;
+        lesser = std::get<stamp_name>(lhs) < std::get<stamp_name>(rhs) ? true :
+                 std::get<stamp_name>(lhs) > std::get<stamp_name>(rhs) ? false : lesser;
+        lesser = std::get<value>(lhs).second < std::get<value>(rhs).second ? true :
+                 std::get<value>(lhs).second > std::get<value>(rhs).second ? false : lesser;
+        lesser = std::get<post_office_name>(lhs) < std::get<post_office_name>(rhs) ? true :
+                 std::get<post_office_name>(lhs) > std::get<post_office_name>(rhs) ? false : lesser;
+        lesser = std::get<year>(lhs) < std::get<year>(rhs) ? true :
+                 std::get<year>(lhs) > std::get<year>(rhs) ? false : lesser;
 
         return lesser;
     }
-
-    bool operator()(const stamp_key lhs,
-                    int rhs) {
-        return std::get<0>(lhs) < rhs;
-    }
-
-    bool operator()(float lhs,
-                    const stamp_key rhs) {
-        return lhs < std::get<0>(rhs);
-    }
 };
 
-struct compare_fuck {
-    int asYear(const stamp_key &key) const {
+struct compare_temp {
+    int as_year(const std::tuple<int, std::string, std::pair<std::string, double>, std::string> &key) const {
         return std::get<0>(key);
     }
 
-    int asYear(int t) const {
+    int as_year(int t) const {
         return t;
     }
 
     template<typename T1, typename T2>
     bool operator()(T1 const &t1,
                     T2 const &t2) const {
-        return asYear(t1) < asYear(t2);
+        return as_year(t1) < as_year(t2);
     }
 };
 
-void insert(stamp_key key,
-            std::set<stamp_key, compare> *stamps) {
-    (*stamps).insert(key);
-}
-
+/*
 bool parse_stamp(const std::string raw_line,
                  std::tuple<int, std::string, std::pair<std::string, double>, std::string>
                  *retval) {
-
 
 }
 
@@ -67,62 +53,38 @@ bool parse_query(const std::string raw_line,
                  std::pair<int, int> *retval) {
 
 }
+*/
 
-bool parse_stamp(stamp_key *key) {
-    if (count < 5) {
-        std::string s1 = "jp2gmd";
-        std::pair<std::string, double> p1("tomatoes", 2.30);
-        stamp_key t1(2137, s1, p1, s1);
-        *key = t1;
-        count++;
-        return true;
-    }
-    return false;
-}
-
-bool parse_query(std::pair<int, int> *query) {
-    if (count < 5) {
-        std::pair<int, int> p1(2120, 2140);
-        *query = p1;
-        ++count;
-        return true;
-    }
-    return false;
-}
-
-void print_stamp(stamp_key stamp) {
+void print_stamp(std::tuple<int, std::string, std::pair<std::string, double>, std::string> stamp) {
     std::cout
-    << std::get<0>(stamp)
-    << std::get<1>(stamp)
-    << std::get<2>(stamp).first
-    << std::get<3>(stamp)
+    << std::get<year>(stamp)
+    << std::get<post_office_name>(stamp)
+    << std::get<value>(stamp).first
+    << std::get<stamp_name>(stamp)
     << std::endl;
 }
 
 void print_stamps(std::pair<int, int> query,
-                  std::set<stamp_key, compare> stamps) {
-    auto it = std::lower_bound(stamps.begin(), stamps.end(), query.first, compare_fuck());
-    auto it2 = std::upper_bound(stamps.begin(), stamps.end(), query.second, compare_fuck());
+                  std::set<std::tuple<int, std::string, std::pair<std::string, double>, std::string>, compare> stamps) {
+    auto it = std::lower_bound(stamps.begin(), stamps.end(), query.first, compare_temp());
+    auto it2 = std::upper_bound(stamps.begin(), stamps.end(), query.second, compare_temp());
 
     std::for_each(it, it2, print_stamp);
 }
 
 int main() {
-    std::set<stamp_key, compare> stamps;
-
+    std::set<std::tuple<int, std::string, std::pair<std::string, double>, std::string>, compare> stamps;
     std::tuple<int, std::string, std::pair<std::string, double>, std::string> stamp;
     std::pair<int, int> query;
-
     bool inserting;
     bool querying = false;
     std::string raw_line;
 
     while (std::getline(std::cin, raw_line)) {
-
         if (!querying) {
             inserting = parse_stamp(raw_line, &stamp);
             if (inserting) {
-                insert(stamp, &stamps);
+                stamps.insert(stamp);
             } else {
                 querying = parse_query(raw_line, &query);
                 print_stamps(query, stamps);
@@ -131,15 +93,14 @@ int main() {
                 fprintf(stderr, "chuj");
             }
         } else {
-
             querying = parse_query(raw_line, &query);
+
             if (querying) {
                 print_stamps(query, stamps);
             } else {
                 fprintf(stderr, "chuj");
             }
         }
-
     }
 
     return 0;
